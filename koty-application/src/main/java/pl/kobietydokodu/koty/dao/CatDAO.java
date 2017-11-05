75d1c97b1fdb60f606d1b5f59ac6329fb5dcda13
shonsu <shonsu82@gmail.com>
2017-11-05 20:09:57 +0100

    changed ID to long type

--- koty-application/src/main/java/pl/kobietydokodu/koty/dao/CatDAO.java
+++ koty-application/src/main/java/pl/kobietydokodu/koty/dao/CatDAO.java
@@ -6,7 +6,7 @@ import pl.kobietydokodu.koty.domain.Cat;
 public interface CatDAO {
 
 	public List<Cat> findAll();
-	public Cat findById(Integer id);
+	public Cat findById(Long id);
 	public void add(Cat kot);
 	public void edit(Cat kot);
 	public void delete(Long idKot);
--- koty-service/src/main/java/pl/kobietydokodu/koty/service/CatService.java
+++ koty-service/src/main/java/pl/kobietydokodu/koty/service/CatService.java
@@ -6,7 +6,7 @@ import pl.kobietydokodu.koty.domain.Cat;
 public interface CatService {
 	
 	public List<Cat> findAll();
-	public Cat findById(Integer id);
+	public Cat findById(Long id);
 	public void add(Cat kot);
 	public void edit(Cat kot);
 	public void delete(Long idKot);
