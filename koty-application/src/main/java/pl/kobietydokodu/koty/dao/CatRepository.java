6489e4b9648db69869acbffe9a081f094fd64c56
shonsu <shonsu82@gmail.com>
2017-11-11 00:06:31 +0100

    organized imports

--- koty-application/src/main/java/pl/kobietydokodu/koty/dao/CatRepository.java
+++ koty-application/src/main/java/pl/kobietydokodu/koty/dao/CatRepository.java
@@ -1,11 +1,9 @@
 package pl.kobietydokodu.koty.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-
 import pl.kobietydokodu.koty.domain.Cat;
 
-@Transactional
+
 public interface CatRepository extends JpaRepository<Cat, Long>  {
 	
 
