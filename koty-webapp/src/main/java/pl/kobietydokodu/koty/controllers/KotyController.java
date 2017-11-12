8b36cd4c84a0f818bb65a096781aed56201caee8
shonsu <shonsu82@gmail.com>
2017-11-12 19:17:34 +0100

    configured logback

--- koty-webapp/src/main/java/pl/kobietydokodu/koty/controllers/KotyController.java
+++ koty-webapp/src/main/java/pl/kobietydokodu/koty/controllers/KotyController.java
@@ -1,7 +1,5 @@
 package pl.kobietydokodu.koty.controllers;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimeZone;
@@ -26,16 +24,17 @@ import org.slf4j.LoggerFactory;
 
 @Controller
 public class KotyController {
-	
+
 	private final Logger logger = LoggerFactory.getLogger(KotyController.class);
-	
+
 	static {
-	    TimeZone.setDefault(TimeZone.getTimeZone("UTC")); //bez tego data zapisywana w bazie danych była cofnięta o jeden dzień.  
-	  }
-	
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC")); // bez tego data zapisywana w bazie danych była cofnięta o
+															// jeden dzień.
+	}
+
 	@Autowired
 	private JpaRepositoryService catService;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String glowny() {
 		return "redirect:/cats";
@@ -43,21 +42,21 @@ public class KotyController {
 
 	@RequestMapping(value = "/cats", method = RequestMethod.GET)
 	public String showAllCats(Model model) {
-		
+
 		logger.debug("showAllCats()");
-		
+
 		model.addAttribute("cats", catService.findAll());
 		return "cats/list";
 	}
 
 	@RequestMapping(value = "/cats/{id}", method = RequestMethod.GET)
 	public String showCat(Model model, @PathVariable("id") long id) {
-		
+
 		logger.debug("showCat() id: {}", id);
-		
-		if(catService.existsById(id)) {
+
+		if (catService.existsById(id)) {
 			model.addAttribute("cat", catService.findById(id).get());
-		}else {
+		} else {
 			model.addAttribute("css", "danger");
 			model.addAttribute("msg", "Cat not found.");
 		}
@@ -67,58 +66,58 @@ public class KotyController {
 	// show update form
 	@RequestMapping(value = "/cats/{id}/update", method = RequestMethod.GET)
 	public String showUpdateCatForm(@PathVariable("id") long id, Model model) {
-		
+
 		logger.debug("showUpdateCatForm() : {}", id);
-		
-		if(catService.existsById(id)) {
+
+		if (catService.existsById(id)) {
 			Cat cat = catService.findById(id).get();
 			model.addAttribute("catDto", cat);
 			return "cats/catform";
-		}else {
-/*			model.addAttribute("cats", catService.findAll());*/
-/*			model.addAttribute("css", "danger");
-			model.addAttribute("msg", "Cat not found.");*/
-			
+		} else {
+			/* model.addAttribute("cats", catService.findAll()); */
+			/*
+			 * model.addAttribute("css", "danger"); model.addAttribute("msg",
+			 * "Cat not found.");
+			 */
+
 		}
-		return  "redirect:/cats";
+		return "redirect:/cats";
 
 	}
-	
-	
-	//update or add Cat
+
+	// update or add Cat
 	@RequestMapping(value = "/cats", method = RequestMethod.POST)
-	public String saveOrUpdateCat(@ModelAttribute("catDto") @Valid KotDTO catDto,
-			BindingResult result, Model model,
+	public String saveOrUpdateCat(@ModelAttribute("catDto") @Valid KotDTO catDto, BindingResult result, Model model,
 			final RedirectAttributes redirectAttributes) {
 
 		logger.debug("saveOrUpdateCat() : {}", catDto);
-		
+
 		if (result.hasErrors()) {
-			//populateDefaultModel(model);
+			// populateDefaultModel(model);
 			return "cats/catform";
 		} else {
-			
+
 			Cat cat = new Cat();
 			// convert from util (KotDto) to sql date (Cat)
-		    java.util.Date utilDate = new java.util.Date();
-		    utilDate = catDto.getBirthDate();
-		    java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-		    cat.setBirthDate(sqlDate);
-		    
-		    cat.setCustId(catDto.getCustId());
-		    cat.setName(catDto.getName());
-		    cat.setOwner(catDto.getOwner());
-		    cat.setWeight(catDto.getWeight());
-		    cat.setSex(catDto.getSex());
-		    cat.setColoring(catDto.getColoring());
+			java.util.Date utilDate = new java.util.Date();
+			utilDate = catDto.getBirthDate();
+			java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+			cat.setBirthDate(sqlDate);
+
+			cat.setCustId(catDto.getCustId());
+			cat.setName(catDto.getName());
+			cat.setOwner(catDto.getOwner());
+			cat.setWeight(catDto.getWeight());
+			cat.setSex(catDto.getSex());
+			cat.setColoring(catDto.getColoring());
 			// Add message to flash scope
 			redirectAttributes.addFlashAttribute("css", "success");
-			if(cat.isNew()){
+			if (cat.isNew()) {
 				catService.save(cat);
-			  redirectAttributes.addFlashAttribute("msg", "Cat added successfully!");
-			}else{
+				redirectAttributes.addFlashAttribute("msg", "Cat added successfully!");
+			} else {
 				catService.save(cat);
-			  redirectAttributes.addFlashAttribute("msg", "Cat updated successfully!");
+				redirectAttributes.addFlashAttribute("msg", "Cat updated successfully!");
 			}
 
 			// POST/REDIRECT/GET
@@ -127,38 +126,37 @@ public class KotyController {
 		}
 
 	}
-	
+
 	@RequestMapping(value = "/cats/add", method = RequestMethod.GET)
-	public String showAddCatForm(@ModelAttribute("catDto") @Valid KotDTO catDto,
-			BindingResult result, Model model) {
-		
+	public String showAddCatForm(@ModelAttribute("catDto") @Valid KotDTO catDto, BindingResult result, Model model) {
+
 		logger.debug("showAddCatForm()");
-		
+
 		return "cats/catform";
 
 	}
-	
+
 	@RequestMapping(value = "/cats/{id}/delete", method = RequestMethod.POST)
 	public String deleteCat(@PathVariable("id") long id, final RedirectAttributes redirectAttributes) {
-		
+
 		logger.debug("deleteCat() : {}", id);
-		
-		if(catService.existsById(id)) {
+
+		if (catService.existsById(id)) {
 			catService.delete(catService.findById(id).get());
 			redirectAttributes.addFlashAttribute("css", "success");
 			redirectAttributes.addFlashAttribute("msg", "Cat is deleted!");
-		}else {
+		} else {
 			redirectAttributes.addAttribute("css", "danger");
 			redirectAttributes.addAttribute("msg", "Cat not found.");
 		}
-		
+
 		return "redirect:/cats";
 	}
-	
+
 	@ModelAttribute("webColoringList")
 	public List<String> populateWebColoringList() {
 
-		//Data referencing for web framework checkboxes
+		// Data referencing for web framework checkboxes
 		List<String> webColoringList = new ArrayList<String>();
 		webColoringList.add("white");
 		webColoringList.add("black");
--- koty-webapp/src/main/java/pl/kobietydokodu/koty/dto/KotDTO.java
+++ koty-webapp/src/main/java/pl/kobietydokodu/koty/dto/KotDTO.java
@@ -13,12 +13,19 @@ import javax.validation.constraints.NotNull;
 
 public class KotDTO {
 
+    @Override
+	public String toString() {
+		return "KotDTO [custId=" + custId + ", name=" + name + ", birthDate=" + birthDate + ", weight=" + weight
+				+ ", owner=" + owner + ", sex=" + sex + ", coloring=" + coloring + "]";
+	}
+
+	private Long custId;
     
     @NotNull(message="Imię nie może być puste")
     @Size(min=2, max=30, message = "Długość musi być od 2 do 30 zanków")
     private String name;
     
-    private Long custId;
+
     
     //@Pattern(regexp="^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
